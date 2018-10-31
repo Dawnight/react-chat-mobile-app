63a2197a20a45040c66b5c30c3498e8d4ff58817
Dawnight <weizhiqimail@foxmail.com>
2018-10-31 21:08:37 +0800

    显示对方的最新聊天内容，并把别人的所有的聊天信息显示出来

--- src/container/MSG/index.js
+++ src/container/MSG/index.js
@@ -1,13 +1,67 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {List} from 'antd-mobile';
 
 class MSG extends Component {
+
+  getLast(arr) {
+    return arr[arr.length - 1];
+  }
+
   render() {
+    const Item = List.Item;
+    const Brief = Item.Brief;
+    const {_id, users} = this.props;
+    let msgGroup = {};
+    const {chatMsg} = this.props;
+    chatMsg.forEach(item => {
+      msgGroup[item.chatId] = msgGroup[item.chatId] || [];
+      msgGroup[item.chatId].push(item);
+    });
+    console.log('chatMsg');
+    console.log(chatMsg);
+    const chatList = Object.values(msgGroup);
+    console.log('chatList');
+    console.log(chatList);
     return (
       <div>
-        MSG Page
+          {
+            chatList.map(k => {
+              const lastItem = this.getLast(k);
+              console.log('lastItem');
+              console.log(lastItem);
+              const targetId = k[0].from === _id ? k[0].to : k[0].from;
+              console.log('users');
+              console.log(users);
+              console.log('targetId');
+              console.log(targetId);
+              const userName = users[targetId] ? users[targetId]['userName'] : '';
+              const avatar = users[targetId] ? users[targetId]['avatar'] : '';
+              return (
+                <List key={lastItem._id}>
+                  <Item
+                    thumb={require(`../../static/avatar/${avatar}.png`)}
+                  >
+                    {lastItem.content}
+                    <Brief>{userName}</Brief>
+                  </Item>
+                </List>
+              )
+            })
+          }
       </div>
     )
   }
 }
 
-export default MSG;
+const mapStateToProps = state => ({
+  chatMsg: state.chat.chatMsg,
+  users: state.chat.users,
+  _id: state.user._id,
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MSG);
